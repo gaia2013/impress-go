c4265dacd79c00ebcaae2b9ee78dd34ef0fabd89
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2019-08-12 16:50:08 +0900

    Receiving uploaded files using FormFile

--- server.go
+++ server.go
@@ -6,10 +6,20 @@ import (
   "net/http"
 )
 
+// func process(w http.ResponseWriter, r *http.Request) {
+//   r.ParseMultipartForm(1024)
+//   fileHeader := r.MultipartForm.File["uploaded"][0]
+//   file, err := fileHeader.Open()
+//   if err == nil {
+//     data, err := ioutil.ReadAll(file)
+//     if err == nil {
+//       fmt.Fprintln(w, string(data))
+//     }
+//   }
+// }
+
 func process(w http.ResponseWriter, r *http.Request) {
-  r.ParseMultipartForm(1024)
-  fileHeader := r.MultipartForm.File["uploaded"][0]
-  file, err := fileHeader.Open()
+  file, _, err := r.FormFile("uploaded")
   if err == nil {
     data, err := ioutil.ReadAll(file)
     if err == nil {
