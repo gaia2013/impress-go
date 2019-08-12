6e6ae9be1ee24b0575ffa845185b0ee4d4955c58
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2019-08-12 16:47:08 +0900

    Receiving uploaded files using MultipartForm fields

--- server.go
+++ server.go
@@ -2,14 +2,20 @@ package main
 
 import (
   "fmt"
+  "io/ioutil"
   "net/http"
 )
 
 func process(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "(1)", r.FormValue("hello"))
-  fmt.Fprintln(w, "(2)", r.PostFormValue("hello"))
-  fmt.Fprintln(w, "(3)", r.PostForm)
-  fmt.Fprintln(w, "(4)", r.MultipartForm)
+  r.ParseMultipartForm(1024)
+  fileHeader := r.MultipartForm.File["uploaded"][0]
+  file, err := fileHeader.Open()
+  if err == nil {
+    data, err := ioutil.ReadAll(file)
+    if err == nil {
+      fmt.Fprintln(w, string(data))
+    }
+  }
 }
 
 func main() {
