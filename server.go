083b3f800c1398e3112f3a8e4da1b68d47478cd5
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2019-08-13 14:01:02 +0900

    Write response header with writeheader

--- server.go
+++ server.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+  "fmt"
   "net/http"
 )
 
 func writeExample(w http.ResponseWriter, r *http.Request) {
   str := `<html><head><title>Go Web Programming</title></head><body><h1>Hello World</h1></body></html>`
-  w.Write([]byte(str))
+	  w.Write([]byte(str))
+}
+
+func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
+  w.WriteHeader(501)
+  fmt.Fprintln(w, "そのようなサービスはありません。ほかを当たってください。")
 }
 
 func main() {
@@ -14,5 +20,25 @@ func main() {
     Addr: "127.0.0.1:8080",
   }
   http.HandleFunc("/write", writeExample)
+  http.HandleFunc("/writeheader", writeHeaderExample)
   server.ListenAndServe()
 }
+
+// package main
+// 
+// import (
+//   "net/http"
+// )
+// 
+// func writeExample(w http.ResponseWriter, r *http.Request) {
+//   str := `<html><head><title>Go Web Programming</title></head><body><h1>Hello World</h1></body></html>`
+//   w.Write([]byte(str))
+// }
+// 
+// func main() {
+//   server := http.Server{
+//     Addr: "127.0.0.1:8080",
+//   }
+//   http.HandleFunc("/write", writeExample)
+//   server.ListenAndServe()
+// }
