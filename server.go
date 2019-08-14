3922a4e2a43c63d01686d5e34b43bd3e1df130e9
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2019-08-14 10:36:40 +0900

    Generate random numbers in the handler

--- server.go
+++ server.go
@@ -3,12 +3,16 @@ package main
 import (
   "net/http"
   "html/template"
+  "math/rand"
+  "time"
 )
 
 func process(w http.ResponseWriter, r *http.Request) {
   t, _ := template.ParseFiles("tmpl.html")
-  t.Execute(w, "Hello World!")
+  rand.Seed(time.Now().Unix())
+  t.Execute(w, rand.Intn(10) > 5)
 }
+
 func main() {
   server := http.Server{
     Addr: "127.0.0.1:8080",
