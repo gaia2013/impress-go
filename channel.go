12f35477954f4bf52f806a60705c274d8ba9eced
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2019-09-18 11:44:57 +0900

    Add Delay

--- channel.go
+++ channel.go
@@ -2,6 +2,7 @@ package main
 
 import(
   "fmt"
+  "time"
 )
 
 func callerA(c chan string) {
@@ -17,6 +18,7 @@ func main() {
   go callerA(a)
   go callerB(b)
   for i := 0; i < 5; i++ {
+    time.Sleep(1 * time.Microsecond)
     select {
       case msg := <-a:
 	fmt.Printf("%s from A\n", msg)
