e6896e313a21bba077073f9f6d425a17c6e1d67d
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2019-09-23 18:13:17 +0900

    close channel

--- channel.go
+++ channel.go
@@ -1,31 +1,38 @@
+
+// チャネルを閉じる
 package main
 
-import(
+import (
   "fmt"
-  "time"
 )
 
 func callerA(c chan string) {
   c <- "Hello World!"
+  close(c) // 1.関数が呼び出されたらチャネルを閉じる
 }
 
 func callerB(c chan string) {
   c <- "Hola Mundo!"
+  close(c) // 1.関数が呼び出されたらチャネルを閉じる
 }
 
 func main() {
   a, b := make(chan string), make(chan string)
   go callerA(a)
   go callerB(b)
-  for i := 0; i < 5; i++ {
-    time.Sleep(1 * time.Microsecond)
+
+  var msg string
+  ok1, ok2 := true, true
+  for ok1 || ok2 {
     select {
-      case msg := <-a:
+    case msg, ok1 = <-a: // 2.チャネルが閉じているとok1とok2はfalseになる
+      if ok1 {
 	fmt.Printf("%s from A\n", msg)
-      case msg := <-b:
+      }
+    case msg, ok2 = <-b: // 2.チャネルを閉じているとok1とok2はfalseになる
+      if ok2 {
 	fmt.Printf("%s from B\n", msg)
-      default:
-	fmt.Println("Default")
+      }
     }
   }
 }
